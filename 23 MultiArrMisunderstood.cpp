3a1cd5b36f42de731f68861e68ebc015dac664da
pkc1088 <84122393+pkc1088@users.noreply.github.com>
2022-09-05 14:21:02 +0900

    Update 23 MultiArrMisunderstood.cpp

--- 23 MultiArrMisunderstood.cpp
+++ 23 MultiArrMisunderstood.cpp
@@ -1,4 +1,38 @@
-#include <stdio.h>
+#include <string.h> 
+#include <stdio.h> 
+#include <stdbool.h>
+#include <stdlib.h>
+#include <malloc.h>
+
+int main(void)
+{
+	int board2[2][2]={0};
+	int i, j, m, n, pm, pn, p, cnt=0;
+	char board1[2][2]={};
+    char s;
+	scanf("%d %d", &m, &n);
+	
+	for(i=0; i<m*n; i++)
+	{
+		scanf(" %c", &s);
+		board1[0][i]=s;
+	}
+    /*
+    1 2
+    3 4
+    */
+	printf("%c %c\n", board1[0][2], board1[1][1]);
+	
+    for(i=0;i<m;i++)
+		for(j=0; j<n; j++)
+			printf("%c", board2[i][j]);
+    
+    char *parr[] = {"one", "two", "three"};
+    printf("%c %c\n", parr[1][2], parr[1][4]);	//string이니깐 [1][4]면 널이나오고 그 외 string 아닌 int나 charac은 연달아 나옴
+    return 0;			
+}
+
+/////////
 
 int main(void)
 {
